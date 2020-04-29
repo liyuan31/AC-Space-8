bf4ced56ef1aee3cf7bb0880efd343731c66d81c
Walden Li <walden@waldens-mbp.attlocal.net>
2020-04-29 08:53:21 -0400

    Initial commitment to a higher-level class that manipulates all the instruction page elements

---
+++ Instructions/InstructionStep.js
@@ -0,0 +1,10 @@
+/*
+    The <InstructionStep> is a newly invented step from the last generation of
+    ACVS. It features animated instructions that is aimed to help participants
+    better understand the task.
+*/
+InstructionStep = class {
+    constructor() {
+
+    }
+}
