66eccd0b41ca21e4109777bc5adc76263a55a93e
Walden Li <walden@waldens-mbp.attlocal.net>
2020-04-30 20:21:23 -0400

    Added generate_data_for_all_squares() to InstructionWidget class

--- Instructions/InstructionWidget.js
+++ Instructions/InstructionWidget.js
@@ -20,13 +20,13 @@ InstructionWidget = class {
         this.magenta = "rgb(254, 0, 254)";
         this.gray = "rgb(105, 105, 105)";
         this.cyan = "rgb(0, 150, 150)";
-        this.allColors = [magenta, gray, cyan];
+        this.colors_rgb = [magenta, gray, cyan];
         //  This string represents the colors and digits of the squares
         this.colors = "212100110212110220222111121020001022100010220020211120";
         this.digits = "978669996768697888887889969677693997967787586886767679";
         //  The data of the display for d3 to use
         this.data = this.generate_data_for_all_squares(
-
+            this.w, this.r, this.cx, this.cy, this.colors_rgb, this.colors, this.digits
         );
     }
 
@@ -41,7 +41,37 @@ InstructionWidget = class {
      * This method is called upon class instantiation, but can also serve as a method to
      * update data.
      */
+    generate_data_for_all_squares(w, r, cx, cy, colors_rgb, colors, digits) {
+
+        const get_a_list_of_colors = function (n) {
+            let result = [];
+            for (let i = 0; i < n; i++) {
+                result.push(colors_rgb[colors[colors.length - 1]]);
+                colors = colors.slice(0, colors.length - 1);
+            }
+            return result;
+        }
+        const get_a_list_of_digits = function (n) {
+            let result = [];
+            for (let i = 0; i < n; i++) {
+                result.push(digits[digits.length - 1]);
+                digits = digits.slice(0, digits.length - 1);
+            }
+            return result;
+        }
     
+        // Helper function for generating a ring of <Square> objects
+        // Given start and end index, return an array of strings representing positions
+        const get_a_list_of_positions = function (startIndex, endIndex) {
+            let result = [];
+            for (let i = startIndex; i <= endIndex; i++) { result.push(`${i}`) }
+            return result;
+        }
+        const data = generate_data_for_one_ring_of_squares(r, 24, cx, cy, w, get_a_list_of_colors(24), get_a_list_of_positions(0, 23), get_a_list_of_digits(24))
+            .concat(generate_data_for_one_ring_of_squares(r * 0.75, 18, cx, cy, w, get_a_list_of_colors(18), get_a_list_of_positions(24, 41), get_a_list_of_digits(18))
+                .concat(generate_data_for_one_ring_of_squares(r * 0.50, 12, cx, cy, w, get_a_list_of_colors(12), get_a_list_of_positions(42, 54), get_a_list_of_digits(12))));
+        this.data = data;
+    }
 
 
     /**
@@ -76,7 +106,7 @@ InstructionWidget = class {
     
             result.push(new Square(x, y, w, w, currentColor, currentPosition, currentDigit));
         }
-        this.data = result;
+        return result;
     }
 
 
