12705f6e3467c56a5e1a6acd14049b09c4608da7
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-01 22:24:45 -0400

    Fixed a bug; type error

--- Instructions/InstructionWidget.js
+++ Instructions/InstructionWidget.js
@@ -22,10 +22,11 @@ InstructionWidget = class {
         this.cyan = "rgb(0, 150, 150)";
         this.colors_rgb = [magenta, gray, cyan];
         //  This string represents the colors and digits of the squares
-        this.colors = "212100110212110220222111121020001022100010220020211120";
+        this.colors = "000000000000000000111111111111111111222222222222222222";
         this.digits = "978669996768697888887889969677693997967787586886767679";
         //  The data of the display for d3 to use
-        this.data = generate_data_for_all_squares(
+        this.data = null;
+        this.generate_data(
             this.w, this.r, this.cx, this.cy, this.colors_rgb, this.colors, this.digits
         );
         //  An array of strings representing lines to be displayed at the top left corner
@@ -46,7 +47,7 @@ InstructionWidget = class {
      */
     set_colors(colors) {
         this.colors = colors;
-        this.data = generate_data_for_all_squares(
+        this.data = generate_data(
             this.w, this.r, this.cx, this.cy, this.colors_rgb, this.colors, this.digits
         );
     }
@@ -66,7 +67,45 @@ InstructionWidget = class {
      * @param {*} colors 
      * @param {*} digits 
      */
-    generate_data_for_all_squares(w, r, cx, cy, colors_rgb, colors, digits) {
+    generate_data(w, r, cx, cy, colors_rgb, colors, digits) {
+
+
+        /**
+        * A helper function. Generate data for d3 to use.
+        *
+        * @param {*} radius : the radius of the ring
+        * @param {*} count : the number of squares on this ring
+        * @param {*} cx : x coord of ring center
+        * @param {*} cy : y coord of ring center
+        * @param {*} w : width (and height) of the square, i.e. the square size
+        * @param {*} colors : a list of strings representing color of each square
+        * @param {*} positions : a list of numbers representing the arbitrary position number of each square
+        * @param {*} digits : a list of numbers representing the digits on each square
+        *
+        * @returns
+        */
+        function generate_data_for_one_ring_of_squares(
+            radius, count, cx, cy, w, colors, positions, digits) {
+
+            let result = [];
+
+            const alpha = 2 * Math.PI / count;
+
+            for (let i = 0; i < count; i++) {
+
+                let currentColor = "";
+                let currentPosition = "";
+                let currentDigit = "";
+                typeof colors === "string" ? currentColor = colors : currentColor = colors[i];
+                typeof positions === "string" ? currentPosition = positions : currentPosition = positions[i];
+                typeof digits === "string" ? currentDigit = digits : currentDigit = digits[i];
+
+                let x = (Math.cos(alpha * i + Math.PI / 2) * radius + cx);
+                let y = (Math.sin(alpha * i + Math.PI / 2) * radius + cy);
+                result.push(new Square(x, y, w, w, currentColor, currentPosition, currentDigit));
+            }
+            return result;
+        }
 
         const get_a_list_of_colors = function (n) {
             let result = [];
@@ -101,46 +140,11 @@ InstructionWidget = class {
 
     /**
      * Helper function. Generate data for d3 to use.
-
-
-
-    /**
-     * A helper function. Generate data for d3 to use.
-     * 
-     * @param {*} radius : the radius of the ring
-     * @param {*} count : the number of squares on this ring
-     * @param {*} cx : x coord of ring center
-     * @param {*} cy : y coord of ring center
-     * @param {*} w : width (and height) of the square, i.e. the square size
-     * @param {*} colors : a list of strings representing color of each square
-     * @param {*} positions : a list of numbers representing the arbitrary position number of each square
-     * @param {*} digits : a list of numbers representing the digits on each square
-     * 
-     * @returns
-     */
-    static generate_data_for_one_ring_of_squares(radius, count, cx, cy, w, colors, positions, digits) {
-        let result = [];
-
-        const alpha = 2 * Math.PI / count;
-
-        for (let i = 0; i < count; i++) {
-
-            currentColor = "";
-            currentPosition = "";
-            currentDigit = "";
-            typeof colors === "string" ? currentColor = colors : currentColor = colors[i];
-            typeof positions === "string" ? currentPosition = positions : currentPosition = positions[i];
-            typeof digits === "string" ? currentDigit = digits : currentDigit = digits[i];
-
-            let x = (Math.cos(alpha * i + Math.PI / 2) * radius + cx);
-            let y = (Math.sin(alpha * i + Math.PI / 2) * radius + cy);
-
-            result.push(new Square(x, y, w, w, currentColor, currentPosition, currentDigit));
-        }
-        return result;
-    }
-
-
+    
+    
+    
+    
+    
     /**
      * 
      * @param {boolean} include_digits 
@@ -203,7 +207,7 @@ InstructionWidget = class {
      * 
      * 
      */
-    acvs_render(include_digits=true) {
+    acvs_render(include_digits = true) {
         const rects = this.rects_selection;
         const data = this.data;
         rects.data(data)
@@ -235,7 +239,8 @@ InstructionWidget = class {
             })
             .attr("id", function (d) { return `sq_${d.no}` })
         .merge(rects)
-            .transition(1000)
+            .transition()
+            .duration(500)
             .attr("width", function (d) { return d.w })
             .attr("height", function (d) { return d.h })
             .attr("x", function (d) { return d.x })
@@ -271,15 +276,15 @@ InstructionWidget = class {
             const w = this.w;
             texts.data(data)
                 .enter().append("text")
-                    .attr("x", (function (d) { return d.x + w / 3.25 + "" }))
-                    .attr("y", (function (d) { return d.y + w / 1.35 + "" }))
-                    .attr("fill", "white")
-                    .attr("class", "ace_pretty_text")
-                    .attr("font-size", w * text_shift + "")
-                    .text(function (d) { return d.digit })
+                .attr("x", (function (d) { return d.x + w / 3.25 + "" }))
+                .attr("y", (function (d) { return d.y + w / 1.35 + "" }))
+                .attr("fill", "white")
+                .attr("class", "ace_pretty_text")
+                .attr("font-size", w * text_shift + "")
+                .text(function (d) { return d.digit })
                 .merge(texts)
-                    .transition()
-                    .attr();
+                .transition()
+                .attr();
             texts.exit().remove();
         }
 
@@ -288,7 +293,7 @@ InstructionWidget = class {
 
     acvs_random_update() {
         this.colors = "000000000000000000111111111111111111222222222222222222".shuffle();
-        this.data = generate_data_for_all_squares(
+        this.data = generate_data(
             this.w, this.r, this.cx, this.cy, this.colors_rgb, this.colors, this.digits
         );
         this.acvs_render();
