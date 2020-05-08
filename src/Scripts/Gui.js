d1ced68306a2e3431f76dd65526df26b699e671f
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-08 16:25:33 -0400

    Tried canvas

--- src/Scripts/Gui.js
+++ src/Scripts/Gui.js
@@ -1,14 +1,15 @@
-ui.html = class {
-    /**
-     * Get a d3 selection of the workspace <div>. Create if not exist.
-     * @version 1.0
-     * @date 20200508
-     */
-    static get_workspace() {
-        const master = d3.select(".master-container");
-        if (master.select(".workspace").empty()) {
-            master.append("div").attr("class", "workspace");
-        }
-        return master.select(".workspace");
+/**
+ * Get a d3 selection of the workspace <div>. Create if not exist.
+ * @version 1.0
+ * @date 20200508
+ */
+function get_workspace() {
+    const master = d3.select(".master-container");
+    if (master.select("#workspace").empty()) {
+        master.append("canvas")
+            .attr("id", "workspace")
+            .attr("width", "600")
+            .attr("height", "800");
     }
+    return master.select("#workspace");
 }
